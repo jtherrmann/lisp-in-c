983af7e669019b07b6cf2f5484459338761f616e
Jake Herrmann <jakeherrmann@posteo.net>
2019-02-23 00:27:55 -0900

    Add a test and a TODO to implement proper test assertions.

--- tests/main.c
+++ tests/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+#include "builtins.h"
+#include "obj.h"
+#include "error.h"
+#include "parse-eval.h"
 #include "setup.h"
 
 
+// TODO: implement proper test assertion functions/macros
 int main() {
     init_setup();
-    printf("no tests");
+    ASSERT(b_equal_pred(parse_eval("123"), get_int(123)));
 }
