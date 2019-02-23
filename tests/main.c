5d9564ce614457f4b5dd47a950e0356136591299
Jake Herrmann <jakeherrmann@posteo.net>
2019-02-23 14:50:02 -0900

    Add test_parse_eval_positive_ints and test_parse_eval_negative_ints.

--- tests/main.c
+++ tests/main.c
@@ -7,8 +7,24 @@
 #include "setup.h"
 
 
-// TODO: implement proper test assertion functions/macros
+void test_parse_eval_positive_ints() {
+    ASSERT(b_equal_pred(parse_eval("0"), get_int(0)));
+    ASSERT(b_equal_pred(parse_eval("1"), get_int(1)));
+    ASSERT(b_equal_pred(parse_eval("123"), get_int(123)));
+    ASSERT(b_equal_pred(parse_eval("981723"), get_int(981723)));
+}
+
+
+void test_parse_eval_negative_ints() {
+    ASSERT(b_equal_pred(parse_eval("-1"), get_int(-1)));
+    ASSERT(b_equal_pred(parse_eval("-425"), get_int(-425)));
+    ASSERT(b_equal_pred(parse_eval("-871312"), get_int(-871312)));
+}
+
+
 int main() {
     init_setup();
-    ASSERT(b_equal_pred(parse_eval("123"), get_int(123)));
+    test_parse_eval_positive_ints();
+    test_parse_eval_negative_ints();
+    printf("All tests passed.");
 }
